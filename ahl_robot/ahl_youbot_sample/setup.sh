752bc885037c6567383525097d2cd29f5837c3b1
Daichi Yoshikawa <daichi.yoshikawa@gmail.com>
2015-04-24 23:31:37 -0700

    Renamed executable files from ahl_youbot_tele_operation and ahl_youbot_viewer to tele_operation and viewer respectively.

--- ahl_robot/ahl_youbot_sample/setup.sh
+++ ahl_robot/ahl_youbot_sample/setup.sh
@@ -2,5 +2,5 @@
 MY_CATKIN_WS=$HOME/Work/catkin_ws
 
 sudo setcap cap_net_raw+ep $MY_CATKIN_WS/devel/lib/hello_world_demo/youBot_HelloWorldDemo
-sudo setcap cap_net_raw+ep $MY_CATKIN_WS/devel/lib/ahl_youbot_sample/ahl_youbot_tele_operation
-sudo setcap cap_net_raw+ep $MY_CATKIN_WS/devel/lib/ahl_youbot_sample/ahl_youbot_viewer
+sudo setcap cap_net_raw+ep $MY_CATKIN_WS/devel/lib/ahl_youbot_sample/tele_operation
+sudo setcap cap_net_raw+ep $MY_CATKIN_WS/devel/lib/ahl_youbot_sample/viewer
