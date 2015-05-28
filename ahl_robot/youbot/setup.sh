2af7e3dcb2a3bc43fb5e1d4358927ccfeb5ca875
Daichi Yoshikawa <daichi.yoshikawa@gmail.com>
2015-05-27 20:06:04 -0700

    Deleted directories in youbot.

--- ahl_robot/youbot/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CATKIN_WS=/home/daichi/Work/catkin_ws
-YOUBOT_DRIVER_PATH=$CATKIN_WS/src/ahl_ros_pkg/ahl_robot/youbot/youbot_driver
-
-sudo setcap cap_net_raw+ep $CATKIN_WS/devel/lib/youbot_driver/base_arm_gripper_test
-sudo setcap cap_net_raw+ep $CATKIN_WS/devel/lib/youbot_driver/displayIpAddress
-export YOUBOT_CONFIG_FOLDER_LOCATION=$YOUBOT_DRIVER_PATH/config
-
