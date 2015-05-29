a0ddfca511f7efc91457d2ebff37316d04c5955c
Daichi Yoshikawa <daichi.yoshikawa@gmail.com>
2015-05-29 14:32:24 -0700

    Fixed member name of gazebo_msgs/ApplyJointEfforts.msg

--- ahl_robot/ahl_youbot_server/test/test.cpp
+++ ahl_robot/ahl_youbot_server/test/test.cpp
@@ -2,33 +2,9 @@
 #include <gazebo_msgs/ApplyJointEfforts.h>
 #include <gazebo_msgs/JointStates.h>
 
-void timerCB(const ros::TimerEvent&)
-{
-  gazebo_msgs::ApplyJointEfforts msg;
-  msg.joint_name.push_back("arm_joint_1");
-  msg.joint_name.push_back("arm_joint_2");
-  msg.joint_name.push_back("arm_joint_3");
-  msg.joint_name.push_back("arm_joint_4");
-  msg.joint_name.push_back("arm_joint_5");
-
-  static unsigned long cnt = 0;
-  double f = 100.0;//5.0 * sin(2.0 * M_PI * 0.2 * cnt * 0.01);
-
-  msg.effort.push_back(f);
-  msg.effort.push_back(f);
-  msg.effort.push_back(f);
-  msg.effort.push_back(f);
-  msg.effort.push_back(f);
-
-  msg.start_time = ros::Time(0);
-  msg.duration = ros::Duration(1);
-  ROS_INFO_STREAM(f);
-  ++cnt;
-}
-
 void callback(const gazebo_msgs::JointStates::ConstPtr& msg)
 {
-
+  ROS_INFO_STREAM(msg->name.size());
 }
 
 int main(int argc, char** argv)
@@ -39,15 +15,19 @@ int main(int argc, char** argv)
   ros::Publisher pub = nh.advertise<gazebo_msgs::ApplyJointEfforts>("/gazebo/apply_joint_efforts", 10);
 
   ros::Rate r(1000.0);
+  ros::Subscriber sub = nh.subscribe("/gazebo/joint_states", 10, callback);
+
+  ros::MultiThreadedSpinner spinner;
+  spinner.spin();
 
   while(ros::ok())
   {
     gazebo_msgs::ApplyJointEfforts msg;
-    msg.joint_name.push_back("arm_joint_1");
-    msg.joint_name.push_back("arm_joint_2");
-    msg.joint_name.push_back("arm_joint_3");
-    msg.joint_name.push_back("arm_joint_4");
-    msg.joint_name.push_back("arm_joint_5");
+    msg.name.push_back("arm_joint_1");
+    msg.name.push_back("arm_joint_2");
+    msg.name.push_back("arm_joint_3");
+    msg.name.push_back("arm_joint_4");
+    msg.name.push_back("arm_joint_5");
 
     static unsigned long cnt = 0;
     double f = 5.0 * sin(2.0 * M_PI * 0.1 * cnt * 0.01);
@@ -65,14 +45,9 @@ int main(int argc, char** argv)
 
     pub.publish(msg);
 
+    ros::spinOnce();
     r.sleep();
   }
-
-  ros::Subscriber sub = nh.subscribe("/gazebo/joint_states", 10, callback);
-  ros::Timer timer = nh.createTimer(ros::Duration(0.01), timerCB);
-
-  ros::MultiThreadedSpinner spinner;
-  spinner.spin();
 
   return 0;
 }
