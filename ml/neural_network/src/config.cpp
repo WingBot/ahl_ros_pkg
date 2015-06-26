e30ad3bfeefbdd1d2b11996fb2ea178f1f4b8282
Daichi Yoshikawa <daichi.yoshikawa@gmail.com>
2015-06-26 16:35:17 -0700

    Added lines about BSD licenses to each files.

--- ml/neural_network/src/config.cpp
+++ ml/neural_network/src/config.cpp
@@ -2,7 +2,7 @@
  *
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2014, Daichi Yoshikawa
+ *  Copyright (c) 2015, Daichi Yoshikawa
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
