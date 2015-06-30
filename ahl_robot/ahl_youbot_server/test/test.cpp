48cf37af6e028cc1f293ca066263a48dcbacd92d
Daichi Yoshikawa <daichi.yoshikawa@gmail.com>
2015-06-29 17:29:44 -0700

    Modified test node for youbot server.

--- ahl_robot/ahl_youbot_server/test/test.cpp
+++ ahl_robot/ahl_youbot_server/test/test.cpp
@@ -36,13 +36,157 @@
  *
  *********************************************************************/
 
+#include <boost/thread.hpp>
+#include <Eigen/Dense>
 #include <ros/ros.h>
-#include <gazebo_msgs/ApplyJointEfforts.h>
-#include <gazebo_msgs/JointStates.h>
+#include <ahl_gazebo_interface/gazebo_interface.hpp>
+#include <ahl_gazebo_interface/exception.hpp>
+#include <ahl_robot/ahl_robot.hpp>
+#include <ahl_robot_controller/exception.hpp>
+#include <ahl_robot_controller/robot_controller.hpp>
+#include <ahl_robot_controller/tasks.hpp>
 
-void callback(const gazebo_msgs::JointStates::ConstPtr& msg)
+using namespace ahl_robot;
+using namespace ahl_ctrl;
+
+boost::mutex mutex;
+RobotPtr robot;
+RobotControllerPtr controller;
+bool updated = false;
+TfPublisherPtr tf_pub;
+TaskPtr gravity_compensation;
+TaskPtr damping;
+TaskPtr joint_control;
+TaskPtr joint_limit;
+TaskPtr position_control;
+TaskPtr orientation_control;
+ahl_gazebo_if::GazeboInterfacePtr gazebo_interface;
+bool initialized = false;
+bool joint_updated = false;
+
+void updateModel(const ros::TimerEvent&)
 {
-  ROS_INFO_STREAM(msg->name.size());
+  try
+  {
+    boost::mutex::scoped_lock lock(mutex);
+    if(joint_updated)
+    {
+      robot->computeBasicJacobian("mnp");
+      robot->computeMassMatrix("mnp");
+      controller->updateModel();
+      updated = true;
+      tf_pub->publish(robot);
+    }
+  }
+  catch(ahl_robot::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+  }
+  catch(ahl_ctrl::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+  }
+  catch(ahl_gazebo_if::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+  }
+}
+
+void control(const ros::TimerEvent&)
+{
+  try
+  {
+    boost::mutex::scoped_lock lock(mutex);
+
+    if(gazebo_interface->subscribed())
+    {
+      Eigen::VectorXd q = gazebo_interface->getJointStates();
+      robot->update("mnp", q);
+      joint_updated = true;
+    }
+
+    if(updated)
+    {
+      static long cnt = 0;
+
+      if(initialized == false)
+      {
+        Eigen::VectorXd qd = Eigen::VectorXd::Constant(robot->getDOF("mnp"), M_PI / 4.0);
+        double sin_val = 1.0;//std::abs(sin(2.0 * M_PI * 0.1 * cnt * 0.001));
+        qd = sin_val * qd;
+        joint_control->setGoal(qd);
+
+        static int reached = 0;
+        if(robot->reached("mnp", qd, 0.03))
+        {
+          ++reached;
+
+          if(reached > 500)
+          {
+            std::cout << "switch to task space control." << std::endl;
+
+            initialized = true;
+            controller->clearTask();
+            controller->addTask(damping, 0);
+            controller->addTask(gravity_compensation, 10);
+            controller->addTask(position_control, 10);
+            controller->addTask(orientation_control, 5);
+            controller->addTask(joint_limit, 100);
+            Eigen::Vector3d xd;
+            xd << 0.25, 0.15, 0.75;
+            position_control->setGoal(xd);
+
+            Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
+            double rad = 0.0;
+            R << cos(rad), 0, sin(rad),
+              0, 1, 0,
+              -sin(rad), 0, cos(rad);
+            orientation_control->setGoal(R);
+          }
+        }
+        else
+        {
+          reached = 0;
+        }
+      }
+      else
+      {
+        Eigen::Vector3d xd;
+        xd << 0.25, 0.15, 0.75;
+        //xd.coeffRef(0) += 0.1 * sin(2.0 * M_PI * 0.2 * cnt * 0.001);
+        //xd.coeffRef(1) += 0.1 * sin(2.0 * M_PI * 0.2 * cnt * 0.001);
+        //xd.coeffRef(2) += 0.1 * sin(2.0 * M_PI * 0.2 * cnt * 0.001);
+
+        position_control->setGoal(xd);
+        Eigen::Matrix3d R = Eigen::Matrix3d::Identity();
+        double rad = M_PI / 4.0 * sin(2.0 * M_PI * 0.2 * cnt * 0.001) + M_PI / 4;
+        R << cos(rad), 0, sin(rad),
+          0, 1, 0,
+          -sin(rad), 0, cos(rad);
+        orientation_control->setGoal(R);
+      }
+
+      ++cnt;
+
+      Eigen::VectorXd tau(robot->getDOF("mnp"));
+      //controller->computeGeneralizedForce(tau);
+
+      //gazebo_interface->applyJointEfforts(tau);
+    }
+
+  }
+  catch(ahl_robot::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+  }
+  catch(ahl_ctrl::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+  }
+  catch(ahl_gazebo_if::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+  }
 }
 
 int main(int argc, char** argv)
@@ -50,41 +194,64 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "youbot_test");
   ros::NodeHandle nh;
 
-  ros::Publisher pub = nh.advertise<gazebo_msgs::ApplyJointEfforts>("/gazebo/apply_joint_efforts", 10);
+  ros::Timer timer_update_model = nh.createTimer(ros::Duration(0.01), updateModel);
+  ros::Timer timer_control = nh.createTimer(ros::Duration(0.001), control);
 
-  ros::Rate r(1000.0);
-  ros::Subscriber sub = nh.subscribe("/gazebo/joint_states", 10, callback);
-
-  ros::MultiThreadedSpinner spinner;
-  spinner.spin();
-
-  while(ros::ok())
+  try
   {
-    gazebo_msgs::ApplyJointEfforts msg;
-    msg.name.push_back("arm_joint_1");
-    msg.name.push_back("arm_joint_2");
-    msg.name.push_back("arm_joint_3");
-    msg.name.push_back("arm_joint_4");
-    msg.name.push_back("arm_joint_5");
+    robot = RobotPtr(new Robot("youbot"));
+    ParserPtr parser = ParserPtr(new Parser());
 
-    static unsigned long cnt = 0;
-    double f = 5.0 * sin(2.0 * M_PI * 0.1 * cnt * 0.01);
+    std::string path = "/home/daichi/Work/catkin_ws/src/ahl_ros_pkg/ahl_robot/ahl_robot/yaml/youbot.yaml";
+    parser->load(path, robot);
 
-    msg.effort.push_back(0);
-    msg.effort.push_back(f);
-    msg.effort.push_back(f);
-    msg.effort.push_back(f);
-    msg.effort.push_back(f);
+    controller = RobotControllerPtr(new RobotController());
+    controller->init(robot, "mnp");
 
-    msg.start_time = ros::Time(0);
-    msg.duration = ros::Duration(0.001);
-    ROS_INFO_STREAM(f);
-    ++cnt;
+    using namespace ahl_gazebo_if;
+    gazebo_interface = GazeboInterfacePtr(new GazeboInterface());
+    gazebo_interface->setDuration(0.010);
+    gazebo_interface->addJoint("youbot::joint1");
+    gazebo_interface->addJoint("youbot::joint2");
+    gazebo_interface->addJoint("youbot::joint3");
+    gazebo_interface->addJoint("youbot::joint4");
+    gazebo_interface->addJoint("youbot::joint5");
+    gazebo_interface->addMobility2D("youbot");
+    gazebo_interface->connect();
 
-    pub.publish(msg);
+    tf_pub = TfPublisherPtr(new TfPublisher());
 
-    ros::spinOnce();
-    r.sleep();
+    ManipulatorPtr mnp = robot->getManipulator("mnp");
+
+    gravity_compensation = TaskPtr(new GravityCompensation(mnp));
+    damping = TaskPtr(new Damping(mnp));
+    joint_control = TaskPtr(new JointControl(mnp));
+    joint_limit = TaskPtr(new JointLimit(mnp, 0.087));
+    position_control = TaskPtr(new PositionControl(mnp, "gripper", 0.001));
+    orientation_control = TaskPtr(new OrientationControl(mnp, "gripper", 0.001));
+
+    controller->addTask(gravity_compensation, 0);
+    controller->addTask(damping, 0);
+    //controller->addTask(joint_control, 0);
+    //controller->addTask(joint_limit, 100);
+
+    ros::MultiThreadedSpinner spinner;
+    spinner.spin();
+  }
+  catch(ahl_robot::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+    exit(1);
+  }
+  catch(ahl_ctrl::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+    exit(1);
+  }
+  catch(ahl_gazebo_if::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+    exit(1);
   }
 
   return 0;
